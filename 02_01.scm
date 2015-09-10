8aadf99aeed2dbb1af77c20ea148603b3e2c2f94
firewood <karamaki@gmail.com>
2015-09-10 20:18:29 +0900

    2.1

--- 02_01.scm
+++ 02_01.scm
@@ -20,3 +20,24 @@
   (= (* (numer x) (denom y))
      (* (numer y) (denom x))))
 
+(define (sign n)
+  (if (< n 0) -1 1))
+
+(define (make-rat n d)
+  (let ((common (* (gcd n d) (sign d))))
+    (cons (/ n common) (/ d common))))
+
+(define (numer x) (car x))
+
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define one-half (make-rat 1 2))
+
+(define one-third (make-rat 1 3))
+
