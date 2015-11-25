96d37c5f202cc2ae745168b01fe8b762a4ce9403
firewood <karamaki@gmail.com>
2015-11-25 20:22:26 +0900

    2.37

---
+++ 02_37.scm
@@ -0,0 +1,34 @@
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (map (lambda (x) (car x)) seqs))
+            (accumulate-n op init (map (lambda (x) (cdr x)) seqs)))))
+
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+  (map (lambda (x) (dot-product x v)) m))
+
+(define (transpose mat)
+  (accumulate-n cons nil mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (x) (matrix-*-vector cols x)) m)))
+
+(define a (list (list 1 2) (list 3 4)))
+
+(define b (matrix-*-vector a (list 5 6)))
+
+(define c (list (list 1 2 3 4) (list 5 6 7 8) (list 9 10 11 12)))
+
+(define d (transpose c))
+
+(define e (matrix-*-matrix a a))
