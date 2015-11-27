db4a08c9e8ab2867b7e00b5001ad2e8a97f7689a
firewood <karamaki@gmail.com>
2015-11-27 20:03:29 +0900

    2.39

---
+++ 02_39.scm
@@ -0,0 +1,27 @@
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op initial (cdr sequence)))))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(define (reverse-r sequence)
+  (fold-right (lambda (x y) (append y (list x))) nil sequence))
+
+(define (reverse-l sequence)
+  (fold-left (lambda (x y) (cons y x)) nil sequence))
+
+(define a (list 1 2 3 4 5))
+
+(define b (reverse a))
+
+(define c (reverse-r a))
+
+(define d (reverse-l a))
