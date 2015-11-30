f0fe4a7ad4c735088bae3e4a1b9663b07f45f8b2
firewood <karamaki@gmail.com>
2015-11-30 20:40:51 +0900

    simplified

--- 02_40.scm
+++ 02_40.scm
@@ -16,16 +16,32 @@
       nil
       (cons low (enumerate-interval (+ low 1) high))))
 
+(define (square n) (* n n))
+
+(define (divides? a b) (= (remainder b a) 0))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
 (define (prime? n)
-  #t)
+  (= n (smallest-divisor n)))
 
-
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cdr pair))))
 
 (define (flatmap proc seq)
   (accumulate append nil (map proc seq)))
 
-(define (prime-sum? pair)
-  (prime? (+ (car pair) (cdr pair))))
+(define (unique-pairs n)
+  (flatmap (lambda (i)
+             (map (lambda (j) (cons i j))
+                  (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
 
 (define (make-pair-sum pair)
   (list (car pair) (cdr pair) (+ (car pair) (cdr pair))))
@@ -40,10 +56,7 @@
                 (enumerate-interval 1 n)))))
 
 (define (prime-sum-pairs n)
-  (filter (lambda (x)
-            (prime? (+ (car x) (cdr x))))
-;            #t)
-          (unique-pairs n)))
+  (filter prime-sum? (unique-pairs n)))
 
 (define (permutations s)
   (if (null? s)
@@ -57,14 +70,6 @@
   (filter (lambda (x) (not (= x item)))
           sequence))
 
-(define (unique-pairs n)
-  (accumulate append
-                      nil
-                      (map (lambda (i)
-                             (map (lambda (j) (list i j))
-                                  (enumerate-interval 1 (- i 1))))
-                           (enumerate-interval 1 n))))
-
 (define a (unique-pairs 5))
 
-(define b (prime-sum-pairs 2))
+(define b (prime-sum-pairs 6))
