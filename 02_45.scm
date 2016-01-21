5e466af6f6353f1c260cc40032874a13f5d1fd7b
firewood <karamaki@gmail.com>
2016-01-21 19:57:08 +0900

    2.45

--- 02_45.scm
+++ 02_45.scm
@@ -21,43 +21,11 @@
   iter)
 
 (define right-split (split beside below))
-
-
-(define (corner-split painter n)
-  (if (= n 0)
-      painter
-      (let ((up (up-split painter (- n 1)))
-            (right (right-split painter (- n 1))))
-        (let ((top-left (beside up up))
-              (bottom-right (below right right))
-              (corner (corner-split painter (- n 1))))
-          (beside (below painter top-left)
-                  (below bottom-right corner))))))
-
-(define (square-limit painter n)
-  (let ((quarter (corner-split painter n)))
-    (let ((half (beside (flip-horiz quarter) quarter)))
-      (below (flip-vert half) half))))
-
-(define (square-of-four tl tr bl br)
-  (lambda (painter)
-    (let ((top (beside (tl painter) (tr painter)))
-          (bottom (beside (bl painter) (br painter))))
-      (below bottom top))))
-
-(define (flipped-pairs painter)
-  (let ((combine4 (square-of-four identity flip-vert
-                                  identity flip-vert)))
-    (combine4 painter)))
-
-(define (square-limit painter n)
-  (let ((combine4 (square-of-four flip-horiz identity
-                                  rotate180 flip-vert)))
-    (combine4 (corner-split painter n))))
-
-(define wave4 (flipped-pairs wave))
-
+(define up-split (split below beside))
 
 (define e einstein)
 
 (define pe (paint e))
+
+(define a (paint (right-split e 2)))
+(define b (paint (right-split1 e 2)))
